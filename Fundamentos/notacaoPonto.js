b039664b1313f52b1c35f4cbc45063379b30581d
Kelvin Faria <KelvinFaria@localhost.localdomain>
2019-12-09 15:16:34 -0300

    update curso

--- Fundamentos/notacaoPonto.js
+++ Fundamentos/notacaoPonto.js
@@ -8,11 +8,15 @@ console.log(obj1.nome);
 
 function Obj(nome){
     this.nome = nome ; //a palavra this, torna a variavel publica , acessivel fora da funcao
+    this.exec = function (){
+        console.log('exec...'); //a palavra this serve para deixar publico uma funcao tb
+    }
 }
 
 const obj2 = new Obj('cadeira');
 const obj3 = new Obj('mesa');
 console.log(obj2.nome);
 console.log(obj3.nome);
+obj3.exec();
 
 
