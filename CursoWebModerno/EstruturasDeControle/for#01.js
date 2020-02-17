8d9cff36601fa37bfe5ace094309368d9300c204
kelvin.michel <kelvin.michel@ngosolucoes.com.br>
2020-02-17 18:07:05 -0300

    updade estruturas de repetiçao

---
+++ CursoWebModerno/EstruturasDeControle/for#01.js
@@ -0,0 +1,10 @@
+let contadordor = 1;
+while(contador <= 10){
+    console.log(`contador = ${contador}`);
+    contador ++
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(let i=1; i< notas.length; i++){
+    console.log(`notas = ${notas[i]}`);
+}
