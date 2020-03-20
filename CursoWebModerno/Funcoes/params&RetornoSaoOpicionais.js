de5ffa7f72c9a8939dac436d981ef3beef307c4e
kelvin.michel <kelvin.michel@ngosolucoes.com>
2020-03-20 16:00:02 -0300

    update

---
+++ CursoWebModerno/Funcoes/params&RetornoSaoOpicionais.js
@@ -0,0 +1,12 @@
+/* exemplo apenas de como a linguagem e flexivel, nao e recomendado usar isto
+no dia a dia, */
+const area = largura*largura;
+if (area > 20){
+console.log(`Valor acima do permitido ${area}`)
+}else{
+    return area
+}
+console.log(area(2,2))
+console.log(area(2))
+console.log(area(2,3,4,5,65))
+console.log(area(5,5))
