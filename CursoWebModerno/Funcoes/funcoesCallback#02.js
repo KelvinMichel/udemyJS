b476829a7609ada910957676696ee5c5236e4637
KelvinMichel <kelvin_michel16@hotmail.com>
2020-05-10 19:38:36 -0300

    funcoes construtoras

--- CursoWebModerno/Funcoes/funcoesCallback#02.js
+++ CursoWebModerno/Funcoes/funcoesCallback#02.js
@@ -7,6 +7,21 @@ for( let i in notas){
     notasBaixas.push(notas[i]);
     }
 }
-console.log(notasBaixas);
+console.log(notasBaixas + " Sem CallBack");
 
 //Com CallBack
+const notasBaixas2 = notas.filter(function (notas){
+        return notas <= 6 ;
+})
+
+console.log(notasBaixas2 + " Com CallBack")
+
+//Com CallBack e Arrow
+const notasBaixas3 = notas.filter( notas => notas <=6);
+console.log( notasBaixas3 + " Com CallBack e Arrow");
+
+//Reaproveitar a Função em varios lugares
+
+const notasReprovados = nota => nota < 6;
+const notasBaixas4 = notas.filter(notasReprovados);
+console.log( notasBaixas4 + " Com Callback, arrow e reaproveitamento de funcao");
---
+++ CursoWebModerno/Funcoes/funcoesCallback#03.js
@@ -0,0 +1,6 @@
+//Exemplo de CallBack no browser, colar no console do navegador.
+
+document.getElementsByTagName('body')[0].onclick = function (e) {
+    console.log('O Evento Ocorreu!')
+}
+
