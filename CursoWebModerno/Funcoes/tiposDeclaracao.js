d7191e3f64a537e74f3ccd83cbe88d5ef1a5bf08
KelvinMichel <kelvin_michel16@hotmail.com>
2020-05-18 18:38:51 -0300

    update curso

---
+++ CursoWebModerno/Funcoes/tiposDeclaracao.js
@@ -0,0 +1,17 @@
+// metodo funcion declaration
+function soma(x,y){
+    return x + y
+}
+console.log(soma(2,3))
+
+//metodo express declaration
+const sub = function(x,y){
+    return x - y
+}
+console.log(sub(2,3))
+
+//metodo express function declaration
+const mult = function mult(x,y){
+    return x * y
+}
+console.log(mult(2,3))
